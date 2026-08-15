a064316f0d3244895741615bbc32036c61894dc8
Adjei Anet Frederic Junior <56155659+difre87@users.noreply.github.com>
2026-08-15 10:02:09 +0000

    feat: add BATIX Growth admin views

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('growth.auth')->group(function () {
     Route::get('/', [GrowthController::class, 'index'])->name('growth.index');
+    Route::get('/campaigns', [GrowthController::class, 'campaigns'])->name('campaigns.index');
+    Route::get('/contents', [GrowthController::class, 'contents'])->name('contents.index');
+    Route::get('/leads', [GrowthController::class, 'leads'])->name('leads.index');
+
     Route::post('/campaigns', [GrowthController::class, 'storeCampaign'])->name('campaigns.store');
     Route::post('/campaigns/{campaign}/generate', [GrowthController::class, 'generate'])->name('campaigns.generate');
     Route::post('/leads', [GrowthController::class, 'storeLead'])->name('leads.store');
